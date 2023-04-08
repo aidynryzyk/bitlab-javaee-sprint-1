d79ea09e487cafa4a9e466da9e1886b29f7c2258
aidyninho <ryzyk.aidyn@gmail.com>
2023-04-08 17:55:51 +0600

    initial commit

---
+++ bitlab-javaee-sprint-1/src/main/java/com/aidyninho/sprint1/servlets/TasksServlet.java
@@ -0,0 +1,20 @@
+package com.aidyninho.sprint1.servlets;
+
+import com.aidyninho.sprint1.db.DBManager;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/tasks")
+public class TasksServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DBManager dbManager = DBManager.getInstance();
+        req.setAttribute("tasks", dbManager.getAllTasks());
+        req.getRequestDispatcher("tasks.jsp").forward(req, resp);
+    }
+}
